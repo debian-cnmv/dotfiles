0252a985e153805510a53b881f28f21d6d5cf039
Alin Nicolescu <profesor@hp.cnmv.local>
2019-12-04 11:05:52 +0200

    Repaired script that creates mariadb users and databases

--- scripts/create_sql_users.sh
+++ scripts/create_sql_users.sh
@@ -1,33 +1,52 @@
 #!/bin/sh
 
+SUCCESS=0
+FAIL=1
+FILE="./users.txt"
+
 create_sql()
 {
-	FILE="./users.txt"
-	
-	if [ -f "$FILE" ];
-	then
-		while IFS=: read -r user pass
-		do
-			printf "CREATE DATABASE IF NOT EXISTS %s;\n" "$user"
-			printf "CREATE OR REPLACE USER '%s'@'localhost' IDENTIFIED BY '%s';\n" "$user" "$pass" 
-			printf "GRANT ALL ON %s.* TO '%s'@'localhost';\n" "$user" "$user" 
-			printf "\n"
-		done <"$FILE"
-		echo "FLUSH PRIVILEGES;"
-		return 0
-	else
-   		echo "File $FILE does not exist in the current directory" >&2
-		echo "Each line of the file must contain a pair like user:password" >&2
-		return 1
-	fi
+	while IFS=: read -r user pass
+	do
+		printf "CREATE DATABASE IF NOT EXISTS %s;\n" "$user"
+		printf "CREATE OR REPLACE USER '%s'@'localhost' IDENTIFIED BY '%s';\n" "$user" "$pass" 
+		printf "GRANT ALL ON %s.* TO '%s'@'localhost';\n" "$user" "$user" 
+		printf "\n"
+	done <"$FILE"
+	echo "FLUSH PRIVILEGES;"
 }
 
 
-create_sql > script.sql
-if [ $? -eq 0 ];
+if [ $# -gt 1 ];
 then
-	# mysql -u root -p < script.sql
-	echo "OK!"
+
+	echo "Error: Too many arguments!"
+	echo "Usage: script-name [users-file]"
+	echo "./users.txt is used if no file is specified"
+	exit $FAIL
 else
-	echo "Fail!"
+	if [ $# -eq 1 ];
+	then
+		FILE="$1"
+	fi
+fi
+
+if [ -f "$FILE" ];
+then
+
+	create_sql > script.sql
+
+	if [ $? -eq 0 ];
+	then
+		mysql -u root -p < script.sql
+		cat ./script.sql
+		rm -f ./script.sql
+		exit $SUCCESS
+	else
+		echo "Something went wrong!"
+		exit $FAIL
+	fi
+else
+	echo "File $FILE not found!"
+	exit $FAIL
 fi
